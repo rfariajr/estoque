81dd5bbd7ff3004f3f7048e883b022e18bd2793e
rfariajr <72213361+rfariajr@users.noreply.github.com>
2024-06-02 16:52:47 -0300

    Add files via upload

--- makeSale/makeSale.js
+++ makeSale/makeSale.js
@@ -2,10 +2,11 @@ document.getElementById("name").onblur = function() {
     var name = document.getElementById("name").value.trim()
     if(name != "") {
         var formData = new FormData(document.getElementById("form"));
-        var url = "http://localhost/estoque2/stockFetchSellVal.php"
+        var url = "http://localhost/estoque2/stockFetch.php"
         fetch(url, {method: "POST", body: formData}).then(response => response.text()).then(queryResult => {
-            if(queryResult < 1) {
+            if(queryResult == 0) {
                 alert("Esse produto não existe no banco de dados.");
+
                 document.getElementById("name").value = "";
                 document.getElementById("qnt").value = "0.00";
                 document.getElementById("sellValue").value = "0.00";
@@ -16,13 +17,42 @@ document.getElementById("name").onblur = function() {
                 document.getElementById("confirmation").disabled = true;
 
                 document.getElementById("confirmation").checked = false;
+
+                document.getElementById("alertNumStock").innerHTML = "";
             }
             else {
-                document.getElementById("sellValue").value = queryResult;
+                var queryResultArray = queryResult.split(";");
 
-                document.getElementById("qnt").disabled = false;
-                document.getElementById("sellValue").disabled = false;                
-                document.getElementById("confirmation").disabled = false;
+                if(queryResultArray[2] <= 0) {
+                    alert("Ação não pode ser realizada. Não há esse unidades desse produto disponível para venda.");
+                    
+                    document.getElementById("name").value = "";
+                    document.getElementById("qnt").value = "0.00";
+                    document.getElementById("sellValue").value = "0.00";
+
+                    document.getElementById("qnt").disabled = true;
+                    document.getElementById("sellValue").disabled = true;
+                    document.getElementById("bttnSend").disabled = true;
+                    document.getElementById("confirmation").disabled = true;
+
+                    document.getElementById("confirmation").checked = false;
+
+                    document.getElementById("alertNumStock").innerHTML = "";
+                }
+
+                else {
+                    document.getElementById("dataFetch").value = queryResult;
+                    document.getElementById("sellValue").value = queryResultArray[3];
+
+                    document.getElementById("alertNumStock").innerHTML = "Quantidade disponível para venda: " + queryResultArray[2];
+
+                    document.getElementById("qnt").max = queryResultArray[2];
+
+                    document.getElementById("qnt").disabled = false;
+                    document.getElementById("sellValue").disabled = false;                
+                    document.getElementById("confirmation").disabled = false;
+                }
+                
             }
         }).catch(error => alert("Error: " + error));
     }
@@ -46,7 +76,9 @@ function resetForm() {
     document.getElementById("bttnSend").disabled = true;
     document.getElementById("confirmation").disabled = true;
 
-    document.getElementById("confirmation").checked = false
+    document.getElementById("confirmation").checked = false;
+
+    document.getElementById("alertNumStock").innerHTML = "";
 
     return true;    
 }
--- makeSale/makeSale.php
+++ makeSale/makeSale.php
@@ -4,12 +4,19 @@
 $name = $_POST["name"];
 $qnt = $_POST["qnt"];
 $sellValue = $_POST["sellValue"];
-$amount = $qnt * $sellValue;
+
+$dataArrayFetch = explode(";", $_POST["dataFetch"]);
+$idProduto = $dataArrayFetch[0];
+$qntStock = $dataArrayFetch[2];
+$valStock = $dataArrayFetch[4];
+
+$qntSub = $qntStock - $qnt;
+$valAdd = $valStock + ($sellValue * $qnt);
 
 //CONECTANDO AO BANCO DE DADOS
 $server = "localhost";
 $user = "adminEstoque";
-$pass = ""
+$pass = "";
 $db = "estoquedb";
 
 $connection = mysqli_connect($server, $user, $pass, $db);
@@ -19,25 +26,14 @@ if(!$connection) {
 }
 
 //ATUALIZANDO TABELA estoque
-$sql = "SELECT id, qnt, valEstoque FROM estoque WHERE nome = '$name'";
-$query = mysqli_query($connection, $sql);
-if(!$query) {
-    die("ERRO!ERRO!ERRO! Erro ao consultar valores no banco de dados: " . mysqli_error($connection));
-}
-
-$result = mysqli_fetch_assoc($query);
-$idProduto = $result["id"];
-$qntSub = $result["qnt"] - $qnt;
-$valEstoque = $result["valEstoque"] + $amount;
-
-$sql = "UPDATE estoque SET qnt = '$qntSub', valEstoque = '$valEstoque' WHERE nome = '$name'";
+$sql = "UPDATE estoque SET qnt = '$qntSub', valEstoque = '$valAdd' WHERE nome = '$name'";
 $query = mysqli_query($connection, $sql);
 if(!$query) {
     die("ERRO!ERRO!ERRO! Erro ao inserir valores no banco de dados: " . mysqli_error($connection));
 }
 
 //PREENCHENDO TABELA registroVendas
-$sql = "INSERT INTO registroVendas (idProduto, qnt, valorVenda) VALUES ('$idProduto', '$qnt', '$amount')";
+$sql = "INSERT INTO registroVendas (idProduto, qnt, valorVenda) VALUES ('$idProduto', '$qnt', '$sellValue')";
 $query = mysqli_query($connection, $sql);
 if(!$query) {
     die("ERRO!ERRO!ERRO! Erro ao inserir valores no banco de dados: " . mysqli_error($connection));
