24758de766efceaffb0dca39b9fd09e98c3715d2
rfariajr <72213361+rfariajr@users.noreply.github.com>
2024-06-05 21:30:30 -0300

    Add files via upload

---
+++ stockFetchNewName.php
@@ -0,0 +1,30 @@
+<?php
+$server = "localhost";
+$user = "adminEstoque";
+$pass = "";
+$db = "estoquedb";
+
+$connection = mysqli_connect($server, $user, $pass, $db);
+if(!$connection) {
+    die("ERRO!ERRO!ERRO! Conexão com banco de dados falhou: " . mysqli_connect_error());
+}
+
+$name = $_POST["newName"];
+$sql = "SELECT * FROM estoque WHERE nome = '$name'";
+$query = mysqli_query($connection, $sql);
+if(!$query) {
+    die("<br>Erro: ".mysqli_error($connection));
+}
+
+if(mysqli_num_rows($query) == 0) {
+    print("0");
+}
+
+else {
+    $row = mysqli_fetch_assoc($query);
+    $response = $row["id"] . ";" . $row["nome"] . ";" . $row["qnt"] . ";" . $row["precoVenda"] . ";" . $row["valEstoque"] . ";" . $row["obs"];
+    print($response);
+}
+
+mysqli_close($connection);
+?>
